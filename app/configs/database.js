fc6785e4e7b67d6e3b5d338584c95c6d4611804a
Cahya Gumilang <cahyagumilang0@gmail.com>
2023-05-08 17:47:54 +0700

    dev

--- app/configs/database.js
+++ app/configs/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "testdb",
+  HOST: process.env.DB_HOST || "localhost",
+  USER: process.env.DB_USER || "root",
+  PASSWORD: process.env.DB_PASSWORD || "root",
+  DB: process.env.DB_NAME || "db_sequelize",
   dialect: "mysql"
 };
